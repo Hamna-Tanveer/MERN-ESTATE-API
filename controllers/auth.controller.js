e17c6faf55c89f2847e6c091d3fa776424f41568
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-08-15 15:36:25 +0500

    update auth Controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,7 +30,12 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = user._doc; // password filed ko rename kr k pass nam rakh do or baqi data ko rest me stroe krwaa don or rest ko respone me dekha do
     res
-      .cookie("token", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+      .cookie("token", token, {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -46,7 +51,14 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "None",
+          secure: true,
+        })
+        .status(200)
+        .json(rest);
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
@@ -63,7 +75,14 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-      res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "None",
+          secure: true,
+        })
+        .status(200)
+        .json(rest);
     }
   } catch (error) {
     next(error);
